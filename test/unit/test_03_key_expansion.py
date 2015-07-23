d2b374530f5fa3c89ffbf13cbf46783d191a7926
au9ustine <duke.augustine@gmail.com>
2015-07-23 21:40:03 +0800

    ISSUE-11 - Add key_expansion for 192 - Austin

--- test/unit/test_03_key_expansion.py
+++ test/unit/test_03_key_expansion.py
@@ -32,4 +32,17 @@ def test_aca_key_expansion_128():
     my_lib.aca_key_expansion(key, key_len, big_w, Nk, Nr)
     actual = struct.unpack('44I', big_w.raw)
     assert actual == expected
+
+def test_aca_key_expansion_192():
+    Nb = c_uint(4)              # Nb = 4
+    key = create_string_buffer('\x8e\x73\xb0\xf7\xda\x0e\x64\x52\xc8\x10\xf3\x2b\x80\x90\x79\xe5\x62\xf8\xea\xd2\x52\x2c\x6b\x7b')
+    key_len = c_uint32(192)     # key_len = 128, 192, 256
+    Nk = c_uint32(6)            # Nk = 4, 6, 8
+    Nr = c_uint32(12)           # Nr = Nk + 6
+    big_w = create_string_buffer(52 * 4) # word w[Nb*(Nr+1)]
+    expected = (0x8e73b0f7, 0xda0e6452, 0xc810f32b, 0x809079e5, 0x62f8ead2, 0x522c6b7b, 0xfe0c91f7, 0x2402f5a5, 0xec12068e, 0x6c827f6b, 0xe7a95b9, 0x5c56fec2, 0x4db7b4bd, 0x69b54118, 0x85a74796, 0xe92538fd, 0xe75fad44, 0xbb095386, 0x485af057, 0x21efb14f, 0xa448f6d9, 0x4d6dce24, 0xaa326360, 0x113b30e6, 0xa25e7ed5, 0x83b1cf9a, 0x27f93943, 0x6a94f767, 0xc0a69407, 0xd19da4e1, 0xec1786eb, 0x6fa64971, 0x485f7032, 0x22cb8755, 0xe26d1352, 0x33f0b7b3, 0x40beeb28, 0x2f18a259, 0x6747d26b, 0x458c553e, 0xa7e1466c, 0x9411f1df, 0x821f750a, 0xad07d753, 0xca400538, 0x8fcc5006, 0x282d166a, 0xbc3ce7b5, 0xe98ba06f, 0x448c773c, 0x8ecc7204, 0x1002202)
+    my_lib.aca_key_expansion(key, key_len, big_w, Nk, Nr)
+    actual = struct.unpack('52I', big_w.raw)
+    assert actual == expected
+    
     
